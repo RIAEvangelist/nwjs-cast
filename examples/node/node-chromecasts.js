67a3c9cfe1b87b9eb30cb25ca22c144f825623f9
Brandon Nozaki Miller <brandon@diginow.it>
2015-01-29 23:59:08 -0800

    fixed timeout and scoping issues

--- examples/node/node-chromecasts.js
+++ examples/node/node-chromecasts.js
@@ -1,11 +1,12 @@
 var nwjsCast=require('../../nwjs-cast');
 var castManager=new nwjsCast();
 
+console.log(nwjsCast, castManager);
 castManager.find(foundDevice);
 
 function foundDevice(chromecast){
     console.log('found chromecast : ',chromecast);
-    
+
     chromecast.on(
         'status',
         function(status){
--- nwjs-cast.js
+++ nwjs-cast.js
@@ -1,114 +1,126 @@
+'use strict';
 var Client = require('castv2-client').Client;
 var DefaultMediaReceiver = require('castv2-client').DefaultMediaReceiver;
 var mdns = require('mdns-js');
 var EventEmitter = require('events').EventEmitter;
 
 /**
+* nwjs-cast chromecast implementation for node-webkit and node.js
+* @module nwjs/cast
+*/
+module.exports=ChromecastManager;
+
+/**
  * Chromecast Manager.
  * @class ChromecastManager
  */
 function ChromecastManager(){
-    
-    
-    this.constructor=init;
-    
-    
-    function init(){
-        Object.defineProperties(
-            this,
-            {
-                /**
-                 * Starts looking for chromecasts
-                 * @method find
-                 * @memberof ChromecastManager
-                 * @param {function} callback - triggered each time a new chromecast device is found.
-                 * @public 
-                 */
-                find:{
-                    value:findChromecasts
-                },
-                /**
-                 * Stops looking for chromecasts
-                 * @method stopFinding
-                 * @memberof ChromecastManager
-                 * @public 
-                 */
-                stopFinding:{
-                    value:stopFindingChromecasts
-                },
-                /**
-                 * Object of found chromecast refrences by name
-                 * @member {Object} devices found on the network
-                 * @memberof ChromecastManager
-                 * @public
-                 */
-                devices:{
-                    value:{},
-                    writeable:true
-                },
-                _manager:{
-                    value:this
-                }
+    Object.defineProperties(
+        this,
+        {
+            find:{
+                value:findChromecasts,
+                enumerable:true
+            },
+            stopFinding:{
+                value:stopFindingChromecasts,
+                enumerable:true
+            },
+            /**
+             * Object of found chromecast refrences by name
+             * @member {Object} devices found on the network
+             * @memberof ChromecastManager
+             * @public
+             */
+            devices:{
+                value:{},
+                writeable:true,
+                enumerable:true
             }
-        );   
-    }
+        }
+    );
     
-    var browser=null;
+    this._browser=null;
     
+    /**
+    * Starts looking for chromecasts
+    * @method find
+    * @memberof ChromecastManager
+    * @param {function} callback - triggered each time a new chromecast device is found.
+    * @public
+    */
     function findChromecasts(callback) {
         if(!callback){
-            throw('findChromecasts requires a callback to trigger when a chromecast is found');   
+            throw('findChromecasts requires a callback to trigger when a chromecast is found');
         }
         
-        if(browser){
-            stopFindingChromecasts();
-            browser=null;
+        if(this._browser){
+            this.stopFinding();
         }
-
-        browser=mdns.createBrowser('_googlecast._tcp');
-        browser.foundCallback=callback;
-        browser._manager=this;
         
-        browser.on(
-            'ready',
-            find
+        this._browser=mdns.createBrowser('_googlecast._tcp');
+        
+        this._browser._manager=this;
+        this._browser.foundCallback=callback;
+        this._browser._chromecastScanTimer=setTimeout(
+            function(){
+                var timer=this;
+                timer.findChromecasts.apply(
+                    timer._browser._manager,
+                    [timer._browser.foundCallback]
+                );
+            },
+            10000
         );
-
-        browser.on(
+        this._browser._chromecastScanTimer.findChromecasts=findChromecasts;
+        this._browser._chromecastScanTimer._browser=this._browser;        
+    
+        this._browser.on(
+            'ready',
+            function(){
+                var browser=this;
+                this.discover();
+            }
+        );
+        this._browser.on(
             'update',
             foundDevice
         );
     }
-    
-    /**
-     * Triggered when browser ready to start searching
-     * @function - starts mdns browser searching
-     * @memberof ChromecastManager
-     * @private
-     */
-    function find(){
-        browser.discover();   
-    }
-    
+
     /**
      * Triggered when service found by mdns browser
      * @function
      * @memberof ChromecastManager
-     * @param {Object} data - mdns network object 
+     * @param {Object} data - mdns network object
      * @private
      */
     function foundDevice(data){
+        
+//        if(data.networkInterface=='pseudo multicast'){
+//            return;
+//        }
+        var browser=this;
         var chromecast = new Chromecast(data);
         if (this._manager.devices[chromecast.name]) {
             return;
         }
-        this._manager.devices[chromecast.name] = chromecast;
+        browser._manager.devices[chromecast.name] = chromecast;
         chromecast._register();
-        this.foundCallback(chromecast);
+        browser.foundCallback(chromecast);
     }
-    
+
+    /**
+    * Stops looking for chromecasts
+    * @method stopFinding
+    * @memberof ChromecastManager
+    * @public
+    */
     function stopFindingChromecasts(){
-        browser.stop();
+        clearTimeout(this._browser._chromecastScanTimer);
+        delete this._browser._chromecastScanTimer;
+        this._browser.stop();
+        this._browser=null;
     }
 }
 
@@ -120,27 +132,28 @@ function ChromecastManager(){
  * @returns {EventEmitter} chromecast object
  */
 function Chromecast(data) {
-    
+
     /**
      * Chromecast Object
      * @member {object} chromecast refrence to a chromecast found on the network
      * @memberof Chromecast
      */
     var chromecast = new EventEmitter();
-    
+
     Object.defineProperties(
         chromecast,
         {
-            /** 
-             * chromecast name 
+            /**
+             * chromecast name
              * @member {string} name of the chromecast device
              * @memberof chromecast
              * @public
              */
             name : {
-                value:data.txt[4].replace('fn=','')
+                value:data.txt[4].replace('fn=',''),
+                enumerable:true
             },
-            /** 
+            /**
              * chromecast session id
              * @member {String} session id last used when connecting to chromecast
              * @memberof chromecast
@@ -148,72 +161,72 @@ function Chromecast(data) {
              */
             session:{
                 value:'',
-                writable:true
+                writable:true,
+                enumerable:true
             },
-            /** 
-             * chromecast status object 
+            /**
+             * chromecast status object
              * @member {Object} status information about the chromecasts current status
              * @memberof chromecast
              * @public
              */
             status:{
                 value:{
-                    /**
-                     * Request the chromecast status
-                     * @method get
-                     * @memberof chromecast.status
-                     * @public
-                     */
-                    get:getStatus 
+                    get:getStatus
                 },
-                writable:true
+                writable:true,
+                enumerable:true
             },
-            /** 
+            /**
              * chromecast ip
              * @member {String} ip of this chromecast
              * @memberof chromecast
              * @public
              */
             ip:{
-                value:data.addresses[0]
+                value:data.addresses[0],
+                enumerable:true
             },
             _networkData:{
                 value:data
             },
             _client:{
-                value:new Client()   
+                value:new Client()
             },
             _register:{
-                value:register   
+                value:register
             }
         }
     );
-    
+
     chromecast._client.on(
         'error',
         function error(err) {
-            chromecast.emit(
-                'error',
-                err
-            );
-            delete chromecast._manager.devices[chromecast.name];
-            chromecast._client.close();
+            this.client.close();
+            console.log(err);
             chromecast.emit('disconnected');
+            delete chromecast._manager.devices[chromecast.name];
             chromecast=null;
         }
     );
-    
+
     function register(){
         chromecast._client.connect(
             chromecast.ip,
             connected
         );
     }
-    
+
+    /**
+    * Request the chromecast status
+    * @method get
+    * @memberof chromecast.status
+    * @public
+    */
     function getStatus(){
         chromecast._client.receiver.getStatus(gotStatus);
     }
-    
+
     /**
      * Triggered when chromecast sends status update. This will update the chromecast status object.
      * @param {Object} status chromecast status info
@@ -226,7 +239,7 @@ function Chromecast(data) {
         }
         chromecast.emit('status',chromecast.status);
     }
-    
+
     /**
      * Triggered with result from getStatus call
      * @memberof getStatus
@@ -237,7 +250,7 @@ function Chromecast(data) {
     function gotStatus(err, status) {
         setStatus(status);
     }
-    
+
     /**
      * Triggered when castv2 client connects to chromecast
      * @memberof Chromecast
@@ -250,12 +263,6 @@ function Chromecast(data) {
         );
         getStatus(gotStatus);
     }
-    
+
     return chromecast;
 }
-
-/**
- * nwjs-cast chromecast implementation for node-webkit and node.js
- * @module nwjs/cast
- */
-module.exports=ChromecastManager;
